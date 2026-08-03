adebcf5de794373be87d4fa83f8f39070e3c5430
guilherme-dds <guilherme.dds.dev@gmail.com>
2026-08-03 19:13:56 -0300

    feat: add auth middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+type TokenPayload = {
+  id: String;
+  iat: number;
+  exp: number;
+};
+
+export function AuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Roken not provided" });
+  }
+
+  const [, token] = authorization.split(" ");
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET!);
+    const { id } = decoded as TokenPayload;
+
+    req.userId = id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Token invalid" });
+  }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserController } from "./controller/UserController";
 import { AuthController } from "./controller/AuthController";
+import { AuthMiddleware } from "./middlewares/auth";
 
 const usercontroller = new UserController();
 const authcontroller = new AuthController();
@@ -8,5 +9,5 @@ const authcontroller = new AuthController();
 export const router = Router();
 
 router.post("/create", usercontroller.store);
-router.get("/users", usercontroller.index);
+router.get("/users", AuthMiddleware, usercontroller.index);
 router.post("/auth", authcontroller.authenticate);
