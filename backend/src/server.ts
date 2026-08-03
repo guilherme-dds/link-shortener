09ab673dfef9b114e9ea2aa811876090a666e93e
guilherme-dds <guilherme.dds.dev@gmail.com>
2026-08-03 13:25:54 -0300

    fix: prisma version

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { router } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
 
-app.listen(3333, () => 
-    console.log("server is running on http://localhost:3333")
+app.listen(3333, () =>
+  console.log("server is running on http://localhost:3333"),
 );
